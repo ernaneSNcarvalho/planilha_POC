66f2deb28620e587936145fbc21c4b1b2976ca54
SouzaCarvalho <ernanecarvalho89@gmail.com>
2021-03-15 19:11:03 -0300

    poc

--- 01-connect.js
+++ 01-connect.js
@@ -1,4 +1,4 @@
-const { GoogleSpreadsheet} = require('google-spreadsheet')
+const { GoogleSpreadsheet } = require('google-spreadsheet')
 const credentials = require('./credentials.json')
 
 const doc = new GoogleSpreadsheet('10L0XGUAgDMyE1et8aw1570J9_shO5_LxtXy_JsxmPfs')
